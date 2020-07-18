a0f2e9197e22697662ffabf46cd09b58b3345adc
mesmaroth <mesmaroth@gmail.com>
2020-07-18 00:16:38 -0700

    v1.1 Added payload deployments

--- duckdeploy.sh
+++ duckdeploy.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-#Author: Robert Sandoval (Mesmaroth)
+# Author: Robert Sandoval (Mesmaroth)
 # Description: Run this script to automate the process of encoding your payload, 
 # organizing it on your machine and deploying it to your rubber ducky's micro-sd card.
 # duckdeploy.sh -f [file] -d [drive_letter] -e [eject?]
 
 
-echo "Duckdeploy v1.0"
+echo "Duckdeploy v1.1"
 echo "--------------------"
 
 script=""
 no_move=$false
 
-while getopts ':hf:d:e:' OPTION; do
+while getopts ':hx:f:d:e:' OPTION; do
     case "$OPTION" in
        h)
         echo "Usage:  duckdeploy.sh"
         echo "   or:  duckdeploy.sh -f [file] -d [drive letter] -e [yes/no]"
         echo 
         echo "Arguments:"
-        echo "-f, -file [file]                The ducky script, this must be in the same path as this script"
-        echo "-d, -drive [drive letter]       The letter of the drive of where the USB is located. Input only the letter"
-        echo "-e, -eject [yes/no]             Wether the drive should be ejected after it has been copied over."
+        echo "-x [payload_folder]      Finds and deploys the payload to the mounted usb from the payloads folder."
+        echo "-f [file]                The ducky script, this must be in the same path as this script."
+        echo "-d [drive letter]        The letter of the drive of where the USB is located. Input only the letter."
+        echo "-e [yes/no]              Wether the drive should be ejected after it has been copied over."
         exit 1
+        ;;
+    x)
+        deploy="$OPTARG"    
         ;;
     f)
         script="$OPTARG"
@@ -34,6 +38,67 @@ while getopts ':hf:d:e:' OPTION; do
         ;;
     esac
 done
+
+function get_drive() {
+    for i in {1..4}
+    do
+        if [[ ! $drive ]]; 
+        then
+            echo "What is the drive parttion of the USB?"
+            read drive_letter
+        else
+            drive_letter=$drive
+        fi
+        drive=$(findmnt /dev/$drive_letter | grep / | cut -d ' ' -f1)
+        if [[ ! -d $drive ]];
+        then
+            echo -e "\e[31mERROR: The drive letter \"$drive_letter\" was not found.\e[0m"
+        else
+            break
+        fi
+
+        if [[ i = 4 ]]
+        then
+            exit 0
+        fi
+    done
+}
+
+function eject_drive() {
+        if [[ ! $eject ]];
+    then
+        echo "Would you like to unmount the drive? (y/n)"
+        read eject
+    fi
+    eject=$(echo $eject | tr '[:upper:]' '[:lower:]')
+
+    if [[ $eject == "y" ]] || [[ $eject == "yes" ]];
+    then
+        sudo umount /dev/$drive_letter
+        echo "Drive \"$drive_letter\" has been unmounted."
+    fi
+}
+
+if [[ $deploy ]];
+then
+    if ! [[ -d "./payloads/$deploy" ]];
+    then
+        echo -e "\e[31mERROR: Folder not found.\e[0m"
+        exit
+    fi
+    if ! [[ -f "./payloads/$deploy/inject.bin" ]];
+    then
+        echo -e "\e[31mERROR: Inject.bin not found in $deploy folder.\e[0m"
+        exit
+    fi
+    get_drive
+    cp ./payloads/$deploy/inject.bin $drive/inject.bin
+    echo "Payload $deploy has been deployed to drive \"$drive\""
+    sleep 1
+    eject_drive
+    echo "DONE"
+    exit
+fi
 
 if ! [[ $script ]]; 
 then
@@ -71,45 +136,10 @@ java -jar duckencoder.jar -i ./payloads/$script_name/$script -o ./payloads/$scri
 payload="./payloads/$script_name/inject.bin"
 echo "Payload: $payload"; echo 
 
-for i in {1..4}
-do
-    if [[ ! $drive ]]; 
-    then
-        echo "What is the drive parttion of the USB?"
-        read drive_letter
-    else
-        drive_letter=$drive
-    fi
-    drive=$(findmnt /dev/$drive_letter | grep / | cut -d ' ' -f1)
-    if [[ ! -d $drive ]];
-    then
-        echo -e "\e[31mERROR: The drive letter \"$drive_letter\" was not found.\e[0m"
-    else
-        break
-    fi
-
-    if [[ i = 4 ]]
-    then
-        exit 0
-    fi
-        
-done
-
+get_drive
 cp $payload $drive
 echo "Deployed payload to $drive/inject.bin"
 sleep 1
-if [[ ! $eject ]];
-then
-    echo "Would you like to unmount the drive? (y/n)"
-    read eject
-fi
-eject=$(echo $eject | tr '[:upper:]' '[:lower:]')
-
-if [[ $eject == "y" ]] || [[ $eject == "yes" ]];
-then
-    sudo umount /dev/$drive_letter
-    echo "Drive \"$drive_letter\" has been unmounted."
-fi
-
+eject_drive
 echo "DONE"
 exit 0
